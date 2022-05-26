1cbe315fb47d00d9b776fe2ee4b6a0513cd807fa
orlandogordon <ogordon1998@gmail.com>
2022-05-26 01:07:05 -0400

    Added new weather icons, Added autocomplete feature, added background images based on weather data, css style updates and dynamic styles for mobile screen sizes

--- script.js
+++ script.js
@@ -2,40 +2,57 @@ const searchBtn = document.querySelector(".search-btn");
 const searchBar = document.querySelector(".search-bar");
 const cityHeader = document.querySelector(".city");
 const tempHeader = document.querySelector(".temp");
+const feelsLikeTempHeader = document.querySelector(".feels-like");
 const iconHeader = document.querySelector(".icon");
 const humidityHeader = document.querySelector(".humidity");
 const windHeader = document.querySelector(".wind");
+const timeHeader = document.querySelector(".time");
 const descHeader = document.querySelector(".description");
+const body = document.querySelector("body");
 const APIkey = "470c20b29e55e22dd7209d5f6689cf6a";
 
 let lat = 0;
 let lon = 0;
-let currentLocation = "austin";
+let currentLocation = "toms river";
 let unit = "imperial";
+let time = "";
 
-fetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=${currentLocation}&appid=${APIkey}&units=${unit}`
-)
-  .then((response) => {
-    if (!response.ok) {
-      alert("No weather found.");
-      throw new Error("No weather found.");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    weather.renderWeather(data);
-  });
+/////////////////////////////////////////
+// Testing the Google Places API
+const searchElement = document.querySelector("[data-city-search]");
+const searchBox = new google.maps.places.SearchBox(searchElement);
+searchBox.addListener("places_changed", () => {
+  const place = searchBox.getPlaces()[0];
+  console.log("listneer activated", place);
+  if (place == null) return;
+  const latitude = place.geometry.location.lat();
+  const longitude = place.geometry.location.lng();
+  console.log(place.formatted_address);
+  weather.coordFetch(latitude, longitude, place.formatted_address);
+});
+
+/////////////////////////////////////////
+/////////////////////////////////////////
+
+// fetch(
+//   `https://api.openweathermap.org/data/2.5/weather?q=${currentLocation}&appid=${APIkey}&units=${unit}`
+// )
+//   .then((response) => {
+//     if (!response.ok) {
+//       alert("No weather found.");
+//       throw new Error("No weather found.");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     weather.renderWeather(data);
+//   });
 
 navigator.geolocation.getCurrentPosition((position) => {
   lat = position.coords.latitude;
   lon = position.coords.longitude;
-  // lat = 57;
-  // lon = -2.15;
   if (lat && lon) weather.coordFetch(lat, lon);
-  console.log(lat, lon);
-  console.log(/^[0-9]+$/.test("fda5"), lat);
 });
 
 let weather = {
@@ -91,12 +108,45 @@ let weather = {
 
   renderWeather: function (data) {
     console.log(data);
-    cityHeader.innerHTML = data.name;
+    // Using the timestamp from the data to determine time of day
+    let timeStamp = data.dt * 1000;
+    timeStamp = new Date(timeStamp);
+    data.dt > data.sys.sunrise && data.dt < data.sys.sunset
+      ? (time = "day")
+      : (time = "night");
+
+    // Determining which image to change the background to based on time of day or weather conditions
+    time === "night"
+      ? (body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?night-sky`)
+      : (body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${data.weather[0].description
+          .split(" ")
+          .join("")})`);
+
+    // Changing city name in the card
+    cityHeader.innerHTML = `Weather in ${
+      searchBar.value ? searchBar.value : data.name
+    }`;
+
+    // Changing temperature in the card
     tempHeader.innerHTML = `${data.main.temp}°F`;
-    iconHeader.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    feelsLikeTempHeader.innerHTML = `Feels Like ${data.main.feels_like}`;
+
+    // Changing the icon shown in the card based on weather conditions
+    iconHeader.src = `img/${time}/${
+      data.weather[0].main ? data.weather[0].main : "Drizzle"
+    }.png`;
+
+    // Changing description in the card
     descHeader.innerHTML = data.weather[0].main;
+
+    // Changing the humidity value in the card
     humidityHeader.innerHTML = `Humidity: ${data.main.humidity}%`;
+
+    // Changing the wind speed value in the card
     windHeader.innerHTML = `Wind Speed: ${data.wind.speed} mph`;
+
+    // Changing the timestamp at the bottom of the card (when the data was retrieved)
+    timeHeader.innerHTML = `As of ${timeStamp.toTimeString()}`;
   },
 };
 
