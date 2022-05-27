3c904247ca93a055e6c4a9b933110d3765d01fa7
orlandogordon <ogordon1998@gmail.com>
2022-05-27 01:44:08 -0400

    Added comments to codes, fixed bug involving weather icon for misty weather, added location as parameter for render functions

--- script.js
+++ script.js
@@ -13,42 +13,14 @@ const APIkey = "470c20b29e55e22dd7209d5f6689cf6a";
 
 let lat = 0;
 let lon = 0;
-let currentLocation = "toms river";
+let currentLocation = "new york";
 let unit = "imperial";
 let time = "";
 
 /////////////////////////////////////////
-// Testing the Google Places API
-const searchElement = document.querySelector("[data-city-search]");
-const searchBox = new google.maps.places.SearchBox(searchElement);
-searchBox.addListener("places_changed", () => {
-  const place = searchBox.getPlaces()[0];
-  console.log("listneer activated", place);
-  if (place == null) return;
-  const latitude = place.geometry.location.lat();
-  const longitude = place.geometry.location.lng();
-  console.log(place.formatted_address);
-  weather.coordFetch(latitude, longitude, place.formatted_address);
-});
 
 /////////////////////////////////////////
-/////////////////////////////////////////
-
-// fetch(
-//   `https://api.openweathermap.org/data/2.5/weather?q=${currentLocation}&appid=${APIkey}&units=${unit}`
-// )
-//   .then((response) => {
-//     if (!response.ok) {
-//       alert("No weather found.");
-//       throw new Error("No weather found.");
-//     }
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     weather.renderWeather(data);
-//   });
-
+// Attempting to get the user's current location (user must agree to prompt)
 navigator.geolocation.getCurrentPosition((position) => {
   lat = position.coords.latitude;
   lon = position.coords.longitude;
@@ -56,28 +28,28 @@ navigator.geolocation.getCurrentPosition((position) => {
 });
 
 let weather = {
-  city: currentLocation,
   searchValue: "",
+  // Determines which function is needed to handle the user's search input
   search: function () {
     this.searchValue = `${searchBar.value}`;
     if (/^[0-9]+$/.test(this.searchValue)) this.zipFetch(this.searchValue);
     else this.cityFetch(this.searchValue);
-    console.log("search function activate");
   },
-  coordFetch: function (lat, lon) {
+  // Fetches weather data based on coordinates input. Best compatible with the Places API. Will become the only Fetch function used in a future update.
+  coordFetch: function (lat, lon, location) {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=${unit}`
     )
       .then((response) => {
         if (!response.ok) {
           alert("No weather found.");
-          throw new Error("No weather found.");
+          throw new Error("No weather found for this location. Try again.");
         }
         return response.json();
       })
-      .then((data) => this.renderWeather(data));
+      .then((data) => this.renderWeather(data, location));
   },
-
+  // Fetches weather data based on city input
   cityFetch: function (city) {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=${unit}`
@@ -85,28 +57,28 @@ let weather = {
       .then((response) => {
         if (!response.ok) {
           alert("No weather found.");
-          throw new Error("No weather found.");
+          throw new Error("No weather found for this location. Try again.");
         }
         return response.json();
       })
-      .then((data) => this.renderWeather(data));
+      .then((data) => this.renderWeather(data, data.name));
   },
-
+  // Fetches weather data based on zip code input
   zipFetch: function (zip) {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?zip=${zip},${"us"}&appid=${APIkey}&units=${unit}`
     )
       .then((response) => {
         if (!response.ok) {
-          alert("No weather found.");
+          alert("No weather found for this location. Try again.");
           throw new Error("No weather found.");
         }
         return response.json();
       })
-      .then((data) => this.renderWeather(data));
+      .then((data) => this.renderWeather(data, data.name));
   },
 
-  renderWeather: function (data) {
+  renderWeather: function (data, location = data.name) {
     console.log(data);
     // Using the timestamp from the data to determine time of day
     let timeStamp = data.dt * 1000;
@@ -114,41 +86,34 @@ let weather = {
     data.dt > data.sys.sunrise && data.dt < data.sys.sunset
       ? (time = "day")
       : (time = "night");
-
     // Determining which image to change the background to based on time of day or weather conditions
-    console.log(data.weather[0].description.split(" ").join(""));
     time === "night"
       ? (body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?night-sky`)
       : (body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${data.weather[0].main}-${time})`);
-
     // Changing city name in the card
-    cityHeader.innerHTML = `Weather in ${
-      searchBar.value ? searchBar.value : data.name
-    }`;
-
+    cityHeader.innerHTML = `Weather in ${location}`;
     // Changing temperature in the card
     tempHeader.innerHTML = `${data.main.temp}°F`;
     feelsLikeTempHeader.innerHTML = `Feels Like ${data.main.feels_like}`;
-
     // Changing the icon shown in the card based on weather conditions
     iconHeader.src = `img/${time}/${
-      data.weather[0].main ? data.weather[0].main : "Drizzle"
+      Math.trunc(data.weather[0].id / 100) !== 7
+        ? data.weather[0].main
+        : "Drizzle"
     }.png`;
-
     // Changing description in the card
     descHeader.innerHTML = data.weather[0].main;
-
     // Changing the humidity value in the card
     humidityHeader.innerHTML = `Humidity: ${data.main.humidity}%`;
-
     // Changing the wind speed value in the card
     windHeader.innerHTML = `Wind Speed: ${data.wind.speed} mph`;
-
     // Changing the timestamp at the bottom of the card (when the data was retrieved)
     timeHeader.innerHTML = `As of ${timeStamp.toTimeString()}`;
   },
 };
 
+/////////////////////////////////////////
+// Event Listeners
 searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
   weather.search();
@@ -158,3 +123,19 @@ searchBar.addEventListener("keyup", function (e) {
   e.preventDefault();
   if (e.key === "Enter") weather.search();
 });
+/////////////////////////////////////////
+// Implementing the Google Places API
+const searchElement = document.querySelector("[data-city-search]");
+const searchBox = new google.maps.places.SearchBox(searchElement);
+
+searchBox.addListener("places_changed", function () {
+  const place = searchBox.getPlaces()[0];
+  if (place == null) return;
+  const latitude = place.geometry.location.lat();
+  const longitude = place.geometry.location.lng();
+  console.log(place.formatted_address);
+  weather.coordFetch(latitude, longitude, place.formatted_address);
+});
+/////////////////////////////////////////
+
+// Other Functions (for future update)
